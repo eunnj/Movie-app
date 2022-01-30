913ed2a287778607bcac8201f0a2e6b2b6c7d59c
user <yenje7@naver.com>
2022-01-30 16:25:28 +0900

    Todolist1

--- src/App.js
+++ src/App.js
@@ -2,36 +2,28 @@ import Button from "./Button"
 import styles from "./App.module.css"
 import { useState,useEffect } from "react";
 
-function Hello(){
-  function byeFn(){
-    console.log("bye :(");
-  }
-  function hiFn(){
-      console.log("created :)");
-      return byeFn;
-    }
-  useEffect(()=>{
-    console.log("hi :)")
-    return ()=>console.log("bye :(")
-  })
-  useEffect(function(){
-    console.log("hi :)")
-    return function(){
-      console.log("bye :(")
-    }
-  },[])
-  return (
-  <h1>Hello</h1>
-  );
-}
+
 function App() {
-  const[showing,setShowing]=useState(false);
-  const onClick=()=>setShowing((prev)=>!prev)
- 
+  const [toDo,setToDo] = useState("");
+  const[toDos,setToDos]=useState([]);
+  const onChange=(event)=>setToDo(event.target.value);
+  const onSubmit=(event)=>{
+    event.preventDefault();
+    if(toDo===""){
+        return;
+    }
+     
+     setToDos(CurrentArray=>[toDo,...CurrentArray]);
+     setToDo("");
+  };
+  console.log(toDos)
   return (
     <div>
-      {showing?<Hello/>:null}
-      <button onClick={onClick}>{showing?"Hide":"Show"}</button>  
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."></input>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }

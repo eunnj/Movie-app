b8dd8a70365c8404b3aefe1e456a9da0ca2626fc
user <yenje7@naver.com>
2022-01-31 00:30:06 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import Button from "./Button"
 import styles from "./App.module.css"
 import { useState,useEffect } from "react";
-
+import Movie from "./components/Movie";
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Home from "./routes/Home"
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading,setLoading] = useState(true);
-  const [movies, setMovies]=useState([])
-  const getMovies = async()=>{
-    const json=await (
-      await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )).json();
-    setMovies(json.data.movies);
-    setLoading(false);  
-  };
-  useEffect(()=>{
-    getMovies();
-  },[])
-  console.log(movies);
   return (
-    <div>
-      {loading?<h1>Loading...</h1>:
-      <div>
-        {movies.map(movie=>(
-        <div key={movie.id}>
-          <img src={movie.medium_cover_image}/>
-          <h2>{movie.title}</h2>
-          <p>{movie.summary}</p>
-          {movie.hasOwnProperty("genres")?
-          <ul>
-            {movie.genres.map((g)=>(
-              <li key={g}>{g}</li>
-            ))}
-          </ul> : null}
-        </div>
-        ))}
-      </div>}
-    </div>
-  );
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/movie" element={<Detail/>}/>
+      <Route path="/hello" element={<h1>hello</h1>}/>
+    </Routes>
+  </Router>
+);
 }
 
 export default App;
